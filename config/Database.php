434eef97d38f1d9f90314410eb14c9935c0d51a4
merp2006fns <merp2006fns@gmail.com>
2025-11-05 10:35:57 -0700

    quitar PDO::ATTR_PERSISTENT

--- config/Database.php
+++ config/Database.php
@@ -23,7 +23,7 @@ class Database
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::ATTR_PERSISTENT => true
+                // PDO::ATTR_PERSISTENT => true
             ]);
         } catch (PDOException $e) {
             throw new Exception('Error de conexión: ' . $e->getMessage());
